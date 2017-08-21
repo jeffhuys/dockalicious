ae279a0f8a81913b9d0b6e53e0b0f89ad8407b0e
Jeff Huijsmans <jeff@bleeve.nl>
2017-08-21 14:18:19 +0200

    server.js

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const os      = require('os')
+const express = require('express')
+const app     = express()
+const moment  = require('moment')
+
+const LISTEN_PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hi from ${os.hostname()}!<br>Uptime: ${forHumans(os.uptime())}`)
+})
+
+app.listen(LISTEN_PORT, () => console.log(`Listening on :${LISTEN_PORT}`))
+
+
+
+/**
+ * Translates seconds into human readable format of seconds, minutes, hours, days, and years
+ * 
+ * @param  {number} seconds The number of seconds to be processed
+ * @return {string}         The phrase describing the the amount of time
+ */
+function forHumans ( seconds ) {
+    var levels = [
+        [Math.floor(seconds / 31536000), 'years'],
+        [Math.floor((seconds % 31536000) / 86400), 'days'],
+        [Math.floor(((seconds % 31536000) % 86400) / 3600), 'hours'],
+        [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'minutes'],
+        [(((seconds % 31536000) % 86400) % 3600) % 60, 'seconds'],
+    ];
+    var returntext = '';
+
+    for (var i = 0, max = levels.length; i < max; i++) {
+        if ( levels[i][0] === 0 ) continue;
+        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
+    };
+    return returntext.trim();
+}
